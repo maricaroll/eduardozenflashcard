c6dafd5020f86e9604a894adfe39415cca9d9166
maricaroll <00001099842931SP@al.educacao.sp.gov.br>
2024-11-21 13:56:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cristiano Ronaldo',
+    'Quantos gols, o Cristiano Ronaldo tem?',
+    '910 gols'
+)
+
+criaCartao(
+    'Neymar',
+    'Por quantos clubes Neymar passou?',
+    '4 clubes, Santos, Barcelona, PSG, Al Hilal'
+)
+
+criaCartao(
+    'Santos',
+    'Quantos campeonatos brasileiros possuí o Santos?',
+    '8 brasileirões'
+)
+
+criaCartao(
+    'Brasil',
+    'Quem é o maior artilheiro da seleção?',
+    'Neymar, 79 gols'
+)
